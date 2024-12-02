212369270e2fcc31ac8aa134a50148ab8d00f059
reafus <ivanreafus@gmail.com>
2024-12-02 15:18:52 +0900

    created spring boot app

---
+++ src/main/java/timofeev/microservice/customer/MicroserviceCustomerApplication.java
@@ -0,0 +1,13 @@
+package timofeev.microservice.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/timofeev/microservice/customer/MicroserviceCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package timofeev.microservice.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
