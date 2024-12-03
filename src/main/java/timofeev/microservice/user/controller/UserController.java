254edc3e1032871306c4dc877064a55ba951e108
reafus <ivanreafus@gmail.com>
2024-12-03 16:29:50 +0900

    muser1 - added UserService and redesigned UserController

--- src/main/java/timofeev/microservice/user/controller/UserController.java
+++ src/main/java/timofeev/microservice/user/controller/UserController.java
@@ -1,12 +1,14 @@
 package timofeev.microservice.user.controller;
 
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import timofeev.microservice.user.exceptions.ResourceNotFoundException;
 import timofeev.microservice.user.model.User;
 import timofeev.microservice.user.repository.UserRepository;
+import timofeev.microservice.user.service.UserService;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -17,29 +19,30 @@ import java.util.Map;
 @RequestMapping("/api/v1")
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService, ModelMapper modelMapper) {
+
+        this.userService = userService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userService.findAll();
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException {
-        User user = userRepository
-                .findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+        User user = userService.findById(userId);
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("/users")
     public User createUser(@Valid @RequestBody User user) {
-        return userRepository.save(user);
+        return userService.save(user);
     }
 
     @PutMapping("/users/{id}")
@@ -47,26 +50,15 @@ public class UserController {
             @PathVariable(value = "id") Long userId, @Valid @RequestBody User userToUpdate)
             throws ResourceNotFoundException {
 
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
-
-        user.setEmail(userToUpdate.getEmail());
-        user.setLastName(userToUpdate.getLastName());
-        user.setFirstName(userToUpdate.getFirstName());
-        user.setUpdatedAt(new Date());
-        final User updatedUser = userRepository.save(user);
+        User updatedUser = userService.updateUser(userId, userToUpdate);
         return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/users/{id}")
-    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws Exception {
-        User user = userRepository
-                .findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
-
-        userRepository.delete(user);
+    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable(value = "id") Long userId) throws Exception {
+        userService.deleteUser(userId);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
-        return response;
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/timofeev/microservice/user/service/UserService.java
@@ -0,0 +1,58 @@
+package timofeev.microservice.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import timofeev.microservice.user.exceptions.ResourceNotFoundException;
+import timofeev.microservice.user.model.User;
+import timofeev.microservice.user.repository.UserRepository;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Service
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(long id) throws ResourceNotFoundException {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + id));
+    }
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public User updateUser(long id, User userToUpdate) throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + id));
+
+        user.setEmail(userToUpdate.getEmail());
+        user.setLastName(userToUpdate.getLastName());
+        user.setFirstName(userToUpdate.getFirstName());
+        user.setUpdatedAt(new Date());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(long id) throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + id));
+        userRepository.delete(user);
+    }
+}
